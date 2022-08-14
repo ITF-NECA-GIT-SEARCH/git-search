7b3b4e21be00c9e809ac65026c746000a5ce9316
MaryamSuleiman <marsulaiman94@gmail.com>
2022-08-14 11:20:37 +0100

    Added Searchbox to the App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
 import users from './users.json'
+import Searchbox from'./Searchbox'
 function App() {  
     
   return (
     
     <div>
-
+      <Searchbox/>
     </div>
   )
 }
