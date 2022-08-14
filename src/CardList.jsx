900e870c4945612b1505aff8023884dce47917ab
Hameedarh <ajibadeolayimika@gmail.com>
2022-08-14 14:11:55 +0100

    done with card and footer component, waiting for merge request

---
+++ src/CardList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Card'
+import './Cardlist.css'
+function CardList() {
+  return (
+    <div className='cardlist-container'>
+        <Card />
+    </div>
+  )
+}
+
+export default CardList
