6bb6f977319fe8155fe0a083748b5cbf7efcb099
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-08-14 10:25:43 +0100

    created the React folder

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+import users from './users.json'
+function App() {  
+    
+  return (
+    
+    <div>
+
+    </div>
+  )
+}
+  
+
+export default App;
